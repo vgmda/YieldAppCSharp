af24fcc371b0ee1fce59a371d31f7bc835742f4a
vgmda <vasilegancin@gmail.com>
2022-09-14 23:57:17 +0100

    Add DataAccess

--- YieldAppCSharp/Program.cs
+++ YieldAppCSharp/Program.cs
@@ -6,13 +6,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Start of the App");
+        Console.WriteLine("*** Start of the App ***");
 
-        // .. 
+        var people = DataAccess.GetPeople();
 
-        Console.WriteLine("End of the App");
+        foreach (var p in people)
+        {
+            Console.WriteLine($"Read{p.FirstName} {p.Lastname}");
+        }
+
+        Console.WriteLine("*** End of the App ***");
 
         Console.ReadLine();
     }
 
+}
+
+public class DataAccess
+{
+    public static IEnumerable<PersonModel> GetPeople()
+    {
+
+
+        yield return new PersonModel("First1", "Last1");
+        yield return new PersonModel("First2", "Last2");
+        yield return new PersonModel("First3", "Last3");
+
+
+    }
 }
